4129bb94153951edc4fba6c706733e05a783a893
Pernilla Uhlin <pernilla@Pernillas-MacBook-Pro.local>
2016-09-26 22:40:26 +0200

    First commit

---
+++ fastlane-playground/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  fastlane-playground
+//
+//  Created by Pernilla Uhlin on 2016-09-26.
+//  Copyright © 2016 Nexus. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
---
+++ fastlane-playgroundTests/fastlane_playgroundTests.swift
@@ -0,0 +1,36 @@
+//
+//  fastlane_playgroundTests.swift
+//  fastlane-playgroundTests
+//
+//  Created by Pernilla Uhlin on 2016-09-26.
+//  Copyright © 2016 Nexus. All rights reserved.
+//
+
+import XCTest
+@testable import fastlane_playground
+
+class fastlane_playgroundTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
